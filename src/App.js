683535d5a0093fea75b48eeecd86f60f61e9bbea
edineipinho <edneyrock@gmail.com>
2023-04-02 17:08:40 -0300

    :sparkles: feat(Select1.js): Created a complete component Select for using in any project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import Select1 from './FormComponents/Select1'
 import Input1 from './FormComponents/Input1'
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
-
+  const [produto, setProduto] = React.useState('');
   return (
-    <div>
-      <Input1 label="Nome" id="nome" type="text" value={nome} setValue={setNome} required />
-      <Input1 label="Email" id="email" type="text" value={email} setValue={setEmail} />
-    </div>
+    <>
+      <Select1 options={['smartphone', 'tablet']} value={produto} setValue={setProduto} />
+      <Input1 id="nome" label="Nome" type="text" value={nome} setValue={setNome} required />
+      <Input1 id="email" label="Email" type="email" value={email} setValue={setEmail} />
+    </>
   )
 }
 
