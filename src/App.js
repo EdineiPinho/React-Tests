7b3fadbac57cec57a51b49bf6fe81850a6c62dc9
edineipinho <edneyrock@gmail.com>
2023-04-02 18:03:31 -0300

    :sparkles: feat(Checkbox1.js): Created a component checkbox such may be used in future projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Select1 from './FormComponents/Select1'
 import Input1 from './FormComponents/Input1'
 import Radio1 from './FormComponents/Radio1';
+import Checkbox1 from './FormComponents/Checkbox1';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
@@ -9,11 +10,20 @@ const App = () => {
   const [produto, setProduto] = React.useState('');
   const [cor, setCor] = React.useState('');
   const [fruta, setFruta] = React.useState('');
+  const [linguagens, setLinguagens] = React.useState([]);
+  const [termo, setTermo] = React.useState([]);
   return (
     <>
+      <h2>Checkbox</h2>
+      <Checkbox1 options={["Li e aceito os termos"]} value={termo} setValue={setTermo} />
+      <br />
+      <Checkbox1 options={["Javascript", "PHP", "Ruby", "Python", "C#"]} value={linguagens} setValue={setLinguagens} />
+      <h2>Radio</h2>
       <Radio1 options={["azul", "vermelho", "verde", "amarelo", "cinza", "preto", "branco"]} name="name" value={cor} setValue={setCor} />
       <Radio1 options={["limão", "laranja", "uva"]} name="fruta" value={fruta} setValue={setFruta} />
+      <h2>Select</h2>
       <Select1 options={['smartphone', 'tablet']} value={produto} setValue={setProduto} />
+      <h2>Tipo Texo</h2>
       <Input1 id="nome" label="Nome" type="text" value={nome} setValue={setNome} required />
       <Input1 id="email" label="Email" type="email" value={email} setValue={setEmail} />
     </>
