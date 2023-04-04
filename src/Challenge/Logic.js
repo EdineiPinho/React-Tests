78020446edf37c21bd6a38fd410e2eb144facc5e
edineipinho <edneyrock@gmail.com>
2023-04-04 09:48:31 -0300

    Nothign to say

---
+++ src/Challenge/Logic.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Logic = () => {
+
+  return null
+}
+
+
+
+export default { Logic }
---
+++ src/Challenge/Screen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logic from 'Logic'
+
+const Screen = () => {
+  return (
+    <div>Screen</div>
+  )
+}
+
+export default Screen
