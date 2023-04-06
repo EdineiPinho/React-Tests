2d7a043ca448615473f638b5f0c3b54f448cdd7c
Edinei Pinho dos Santos <edneyrock@gmail.com>
2023-04-06 12:33:24 -0300

    :sparkles: feat: knowing prop-stypes  and .defaultProps how they work

---
+++ src/Mais React/PropTypes/Screen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+
+const Screen = () => {
+  return (
+    <div>
+      <Button width={200}>Clique aqui</Button>
+    </div>
+  )
+}
+
+export default Screen
